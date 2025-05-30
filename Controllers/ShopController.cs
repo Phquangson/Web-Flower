0395f3f960610b8f38b04b8a4f7507e9f3e2affe
Phquangson <Phquangson2003@gmail.com>
2025-05-30 13:57:18 +0700

    Update Shop

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Template_Flower.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Shop()
+        {
+            return View();
+        }
+
+        public  ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
