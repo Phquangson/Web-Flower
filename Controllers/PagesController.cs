d9e26990b41e68510f84350ab0c00927f831235e
Phquangson <Phquangson2003@gmail.com>
2025-06-02 11:02:17 +0700

    Update

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Win32;
+
+namespace Template_Flower.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Pages
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+            public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
